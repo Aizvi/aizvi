f5afd6154ecb7f806f0190ec6599600e0275499c
ahmadhuss <ahmadnet600@gmail.com>
2020-07-05 22:10:04 +0500

    refactor:  next.config.js has been updated

--- next.config.js
+++ next.config.js
@@ -5,20 +5,6 @@ const withPlugins = require('next-compose-plugins');
 const withOffline = require('next-offline');
 const withCSS = require('@zeit/next-css');
 const withSass = require('@zeit/next-sass');
-require('dotenv').config();
-
-
-// Configuration
-const config = {
-	env: {
-		// Reference a variable that was defined in the .env file and make it available at
-		// Project Build Time / Frontend
-		appName: process.env.APP_NAME,
-		appUrl: process.env.APP_URL,
-		appPort: process.env.APP_PORT
-	}
-};
-
 
 // Configuration Try to install app via chrome
 const offlineConfig = {
@@ -51,13 +37,4 @@ const offlineConfig = {
 
 
 
-module.exports = withPlugins([[withCSS],[withSass],[withOffline,offlineConfig]], config);
-
-
-//module.exports = config;
-
-// module.exports = withSass({
-// 	webpack(config, options) {
-// 		return config;
-// 	}
-// });
+module.exports = withPlugins([withCSS,withSass], withOffline(offlineConfig));
